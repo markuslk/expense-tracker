f6c4b2b29819a8112c5ff86fee83961a93a45b5d
Markus <markuslokk@gmail.com>
2024-06-05 16:56:18 +0300

    add rpc for project, refactor api routes, create initial query for total-spent data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,28 @@
+import { api } from "./lib/api";
+import { useQuery } from "@tanstack/react-query";
+
+async function getTotalSpent() {
+  const res = await api.expenses["total-spent"].$get();
+  if (!res.ok) {
+    throw new Error("server error");
+  }
+  const data = await res.json();
+  return data;
+}
+
 function App() {
-  return <></>;
+  const { isPending, error, data } = useQuery({
+    queryKey: ["get-total-spent"],
+    queryFn: getTotalSpent,
+  });
+
+  if (error) return "An error has occured:" + error.message;
+
+  return (
+    <div className="mx-auto p-20 text-5xl">
+      Total spent: {isPending ? 0 : data.total}€
+    </div>
+  );
 }
 
 export default App;
--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,17 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expensesRoute } from "./routes/expenses";
+import { serveStatic } from "@hono/node-server/serve-static";
 
 const app = new Hono();
 
 app.use("*", logger());
 
-app.get("/", (c) => {
-	return c.json({ message: "test" });
-});
+const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute);
 
-app.route("/api/expenses", expensesRoute);
+app.use("*", serveStatic({ root: "./frontend/dist" }));
+app.use("*", serveStatic({ path: "./frontend/dist/index.html" }));
 
 export default app;
+
+export type ApiRoutes = typeof apiRoutes;
