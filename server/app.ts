2eb83039742687e7f5fa7365718bfe9f250851b3
Markus <markuslokk@gmail.com>
2024-06-07 17:54:28 +0300

    create auth route, add to hono

--- server/app.ts
+++ server/app.ts
@@ -2,12 +2,13 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expensesRoute } from "./routes/expenses";
 import { serveStatic } from "@hono/node-server/serve-static";
+import { authRoute } from "./routes/auth";
 
 const app = new Hono();
 
 app.use("*", logger());
 
-const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute);
+const apiRoutes = app.basePath("/api").route("/", authRoute).route("/expenses", expensesRoute);
 
 app.use("*", serveStatic({ root: "./frontend/dist" }));
 app.use("*", serveStatic({ path: "./frontend/dist/index.html" }));
