e46badc3104779dd322f855b17f04c9829206c5e
Markus <markuslokk@gmail.com>
2024-06-11 00:23:29 +0300

    create custom function to format dateString value for frontend

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDateString(dateString: string) {
+  const [year, month, date] = dateString.split("-");
+  return `${date}.${month}.${year}`;
 }
