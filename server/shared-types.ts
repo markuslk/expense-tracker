8cc2304c78e6518a166e3af424a24f39058839e5
Markus <markuslokk@gmail.com>
2024-06-11 01:12:27 +0300

    create CreateExpense type

--- server/shared-types.ts
+++ server/shared-types.ts
@@ -1,3 +1,6 @@
 import { insertExpensesSchema } from "./db/schema/expenses";
+import { z } from "zod";
 
-export const createExpenseSchema = insertExpensesSchema.omit({ userId: true, createdAt: true });
+export const createExpenseSchema = insertExpensesSchema.omit({ userId: true, createdAt: true, id: true });
+
+export type CreateExpense = z.infer<typeof createExpenseSchema>;
